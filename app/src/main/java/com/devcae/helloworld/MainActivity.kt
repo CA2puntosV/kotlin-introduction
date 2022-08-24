db81fc2b7deda4ec2e73b715d1c9134e7486ad4c
Cristian Acevedo <cristian.acevedo@ibacrea.com>
2022-08-24 17:27:47 -0500

    variables and const

--- app/src/main/java/com/devcae/helloworld/MainActivity.kt
+++ app/src/main/java/com/devcae/helloworld/MainActivity.kt
@@ -7,5 +7,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        variablesAndConst()
+    }
+
+    /*VARIABLES AND CONSTANTS
+    var = Las variables después de definirlas podemos cambiar su valor (reasignación/mutable).
+    val = Las constantes no cambian su valor después de ser difinidas (inmutable).
+*/
+    private fun variablesAndConst(){
+        //VARIABLES
+        var myFirstVariable = "Variable Hello World!"
+        println(myFirstVariable)
+
+        //Here myFirstVariable was reassigned
+        myFirstVariable = "Welcome to my first Kotlin app!"
+        println(myFirstVariable)
+
+        //CONSTANTS
+        val myFirstConstant = "Constant Hello World"
+        println(myFirstConstant)
+
+        //myFirstConstant = "Reasinación" (Val cannot be reassigned)
     }
 }
