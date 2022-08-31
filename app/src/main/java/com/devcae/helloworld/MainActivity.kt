baeeb48af2d019d90812df3842f1ccbc3b37ef22
devcae <cristian2puntosv@gmail.com>
2022-08-31 16:17:18 -0500

    added null safety

--- app/src/main/java/com/devcae/helloworld/MainActivity.kt
+++ app/src/main/java/com/devcae/helloworld/MainActivity.kt
@@ -8,19 +8,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //Lesson 1
-        //variablesAndConst()
+        variablesAndConst()
         //Lesson 2
-        //dataTypes()
+        dataTypes()
         //Lesson 3
-        //ifStatement()
+        ifStatement()
         //Lesson 4
-        //whenSentence()
+        whenSentence()
         //Lesson 5
-        //arrays()
+        arrays()
         //Lesson 6
-        //maps()
+        maps()
         //Lesson 7
-        //loops()
+        loops()
         //Lesson 8
         nullSafety()
     }
